4a0cd81457f5db0cc7c65aa8975fb41139bf0331
Craig Hauger <chauger@add123.com>
2018-07-24 16:49:04 -0400

    fixed gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -6,30 +6,30 @@ var concat = require('gulp-concat')
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
-  return gulp.src(['src/scss/*.scss'])
+  return gulp.src(['src/scss/**/*.scss'])
     .pipe(sass())
     .pipe(gulp.dest('src/css'))
     .pipe(browserSync.stream())
 })
 
 // Compile all CSS and JS into Single File
-gulp.task('concatCSS', function (){
+gulp.task('concatCSS', function () {
   return gulp.src([
     'src/css/bootstrap.min.css',
     'src/css/mdb.css'
   ])
-  .pipe(concat('addfldl.css'))
-  .pipe(minify())
-  .pipe(gulp.dest('src/css'))
+    .pipe(concat('addfldl.css'))
+    .pipe(minify())
+    .pipe(gulp.dest('src/css'))
 })
 
-gulp.task('concatJS', function (){
+gulp.task('concatJS', function () {
   return gulp.src([
     'src/js/jquery-3.3.1.min.js',
     'src/js/mdb.min.js'
   ])
-  .pipe(concat('addfldl.js'))
-  .pipe(gulp.dest('src/js'))
+    .pipe(concat('addfldl.js'))
+    .pipe(gulp.dest('src/js'))
 })
 
 // Static Server + watching scss/html files
@@ -43,4 +43,4 @@ gulp.task('serve', ['sass'], function () {
   gulp.watch('src/*.html').on('change', browserSync.reload)
 })
 
-gulp.task('default', ['concatCSS','concatJS','serve'])
+gulp.task('default', ['concatCSS', 'concatJS', 'serve'])
